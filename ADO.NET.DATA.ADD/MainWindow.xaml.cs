aff8f2e2f4f91a7b5a4874d9457fdd0361c34d24
Mehdi_ha01 <Mehdi_ha01@STHQ0116-08>
2023-05-25 08:17:33 +0400

    Add project files.

---
+++ ADO.NET.DATA.ADD/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System.Data.SqlClient;
+using System.Text;
+using System.Windows;
+
+namespace ADO.NET.DATA.ADD
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetDataFromDatabase();
+        }
+
+        private void GetDataFromDatabase()
+        {
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Humans;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            string query = "SELECT FirstName, LastName FROM Human";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    StringBuilder fullNameBuilder = new StringBuilder();
+
+                    while (reader.Read())
+                    {
+                        string name = reader.GetString(reader.GetOrdinal("FirstName"));
+                        string surname = reader.GetString(reader.GetOrdinal("LastName"));
+                        string fullName = $"{name + " " + surname}\n";
+                        fullNameBuilder.AppendLine(fullName);
+
+                    }
+
+                    textBlock.Text = fullNameBuilder.ToString();
+                }
+
+                reader.Close();
+            }
+        }
+        private void AddDataToDatabase(string name, string surname)
+        {
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Humans;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            string query = "INSERT INTO Human (FirstName, LastName) VALUES (@Name, @Surname)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Name", name);
+                command.Parameters.AddWithValue("@Surname", surname);
+                connection.Open();
+                int rowsAffected = command.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("ADD.OK.");
+                    GetDataFromDatabase();
+                }
+            }
+        }
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            string addname1 = addname.Text;
+            string addsurname1 = addsurname.Text;
+            AddDataToDatabase(addname1, addsurname1);
+
+        }
+    }
+}
+
